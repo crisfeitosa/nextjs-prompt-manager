7e11509f6ca7876cdb5f952c8a1fd3bf7a63a996
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-06-03 20:00:54 -0300

    fix: ensure TextEncoder and TextDecoder are defined in jest setup for compatibility

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,9 @@
 import '@testing-library/jest-dom';
 
+if (typeof TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util');
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}
+
 expect.extend({});
